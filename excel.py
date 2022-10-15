37ffeb742ccc9ad932f5e822981d488aec7496e5
Henrique Vital Carvalho <48650626+henriquevital00@users.noreply.github.com>
2022-10-15 13:50:38 -0300

    Fixed excel

--- excel.py
+++ excel.py
@@ -1,4 +1,5 @@
 import xlsxwriter
+import numpy as np
 
 class Excel:
     def __init__(self) -> None:
@@ -6,45 +7,38 @@ class Excel:
         self.worksheet = self.workbook.add_worksheet()
 
     def generate_excel(self, ct_data: dict, network_data: dict) -> None:
-        col = 1
-        row = 0
+        col = 0
+        row = 1
+        media_geral = []
         for key, value in ct_data.items():
-            row = 0
+            col = 0
             ed = value['euclidean_distance']
+            key = key.split("_")[0]
             self.worksheet.write(row, col, key)
-            row = 2
-            for i in range(9):
-                print(ed[8])
-                self.worksheet.write(row, col, ed[i])
-                row += 1
-            self.worksheet.write(row, col, value['mae'])
             col += 1
+            for i in range(9):
+                self.worksheet.write(row, col, float("{:.3f}".format(ed[i])))
+                media_geral.append(ed[i])
+                col += 1
+            self.worksheet.write(row, col, float("{:.3f}".format(value['mae'])))
+            row += 1
 
-        self.worksheet.write(row + 2, 1, network_data['eigvec_per'])
-        self.worksheet.write(row + 3, 1, network_data['box_size'])
-        self.worksheet.write(row + 4, 1, network_data['max_test_steps'])
-        self.worksheet.write(row + 5, 1, network_data['num_random_init'])
-        self.worksheet.write(row + 6, 1, network_data['predict_mode'])
-        self.worksheet.write(row + 7, 1, network_data['shape_model_file'])
+        self.worksheet.write(1, col + 2, float("{:.3f}".format(np.mean(media_geral))))
+        self.worksheet.write(1, col + 3, network_data['eigvec_per'])
+        self.worksheet.write(1, col + 4, network_data['box_size'])
+        self.worksheet.write(1, col + 5, network_data['max_test_steps'])
+        self.worksheet.write(1, col + 6, network_data['num_random_init'])
+        self.worksheet.write(1, col + 7, network_data['predict_mode'])
+        self.worksheet.write(1, col + 8, network_data['shape_model_file'])
         self.workbook.close()
 
     def generate_header(self):
-        row = 1
-        col = 0
-        self.worksheet.write(row, col, 'Distancia euclidiana')
-        self.worksheet.write(row+1, col, 'P1')
-        self.worksheet.write(row+2, col, 'P2')
-        self.worksheet.write(row+3, col, 'P3')
-        self.worksheet.write(row+4, col, 'P4')
-        self.worksheet.write(row+5, col, 'P5')
-        self.worksheet.write(row+6, col, 'P6')
-        self.worksheet.write(row+7, col, 'P7')
-        self.worksheet.write(row+8, col, 'P8')
-        self.worksheet.write(row+9, col, 'P9')
-        self.worksheet.write(row+10, col, 'MAE')
-        self.worksheet.write(row+12, col, 'eigvec_per')
-        self.worksheet.write(row+13, col, 'box_size')
-        self.worksheet.write(row+14, col, 'max_test_steps')
-        self.worksheet.write(row+15, col, 'num_random_init')
-        self.worksheet.write(row+16, col, 'predict_mode')
-        self.worksheet.write(row+17, col, 'shape_model_file')
+        header = ['P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7', 'P8', 'P9', 'MAE', 'media geral', 'eigvec_per',
+                  'box_size', 'max_test_steps', 'num_random_init', 'predict_mode', 'shape_model_file']
+        row = 0
+        col = 1
+        for label in header:
+            self.worksheet.write(row, col, label)
+            col += 1
+            if label == "MAE":
+                col += 1
